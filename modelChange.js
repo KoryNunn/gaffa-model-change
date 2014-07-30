fb2f3277b6ab6d5821eb80d67f3ad35391653c9a
KoryNunn <knunn187@gmail.com>
2014-07-30 13:23:34 +1000

    Bugfix

--- modelChange.js
+++ modelChange.js
@@ -1,30 +1,29 @@
 var Gaffa = require('gaffa');
 
 function change(behaviour){
-        var gaffa = behaviour.gaffa;
+    var gaffa = behaviour.gaffa;
 
-        if(!behaviour.condition.value){
-            return;
-        }
+    if(!behaviour.condition.value){
+        return;
+    }
 
-        var throttleTime = behaviour.throttle;
-        if(!isNaN(throttleTime)){
-            var now = new Date();
-            if(!behaviour.lastTrigger || now - behaviour.lastTrigger > throttleTime){
-                behaviour.lastTrigger = now;
-                behaviour.triggerActions('change');
-            }else{
-                clearTimeout(behaviour.timeout);
-                behaviour.timeout = setTimeout(function(){
-                        behaviour.lastTrigger = now;
-                        behaviour.triggerActions('change');
-                    },
-                    throttleTime - (now - behaviour.lastTrigger)
-                );
-            }
-        }else{
+    var throttleTime = behaviour.throttle;
+    if(!isNaN(throttleTime)){
+        var now = new Date();
+        if(!behaviour.lastTrigger || now - behaviour.lastTrigger > throttleTime){
+            behaviour.lastTrigger = now;
             behaviour.triggerActions('change');
+        }else{
+            clearTimeout(behaviour.timeout);
+            behaviour.timeout = setTimeout(function(){
+                    behaviour.lastTrigger = now;
+                    behaviour.triggerActions('change');
+                },
+                throttleTime - (now - behaviour.lastTrigger)
+            );
         }
+    }else{
+        behaviour.triggerActions('change');
     }
 }
 
